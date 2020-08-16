b80268656a82acb5c9ac739307555f5fc2152dec
faatemeh-chamani <64579048+faatemeh-chamani@users.noreply.github.com>
2020-08-16 06:31:09 +0000

    Update LU-decomposition-partial-pivoting.py

--- LU-decomposition-partial-pivoting.py
+++ LU-decomposition-partial-pivoting.py
@@ -20,24 +20,24 @@ x = np.zeros(n)
 P = np.zeros((n-1, n, n))
 M = np.zeros((n-1, n, n))
 m = 0
-
+print("--------------------------------------")
 for k in range(0, n - 1):
-    Max = abs(A[k, k, k])
+    Max = abs(A[k, k, k])                            # Pivot Element
     for i in range(k + 1, n):
-        if abs(A[k, i, k]) > Max:
+        if abs(A[k, i, k]) > Max:                    # Find The Maximum Element
             Max = A[k, i, k]
             m = i
     print("Max value in step", k + 1, "is : ", A[k, m, k])
     print("row", k, "should replace by row", m)
 
-    P[k, :, :] = np.eye(n, n)
+    P[k, :, :] = np.eye(n)                           # Replace Row k by Row m
     temp = P[k][m, :].copy()
     P[k][m, :] = P[k][k, :].copy()
     P[k][k, :] = temp.copy()
     print("The Permutation Matrix P(k) In Step", k + 1, "Is: \n", P[k, :, :])
 
     A[k+1, :, :] = P[k, :, :].dot(A[k, :, :])
-    M[k, :, :] = np.eye(n, n)
+    M[k, :, :] = np.eye(n)
     for j in range(k + 1, n):
         M[k, j, k] = -float(A[k+1, j, k] / A[k+1, k, k])
     print("Matrix M include Multipliers in step", k + 1, "is:\n", M[k, :, :])
@@ -64,6 +64,7 @@ print("The Unit Lower Triangular Matrix L:\n", L)
 print("MA - U:\n", MM.dot(A[0, :, :]) - U)
 print("PA - LU:\n", np.dot(PP, A[0, :, :]) - np.dot(L, U))
 #####################################################################################
+print("--------------------------------------")
 # MA = U
 # Ax = b
 b1 = np.dot(MM, b)                                                 # b1 = Mb
@@ -79,7 +80,7 @@ for i in range(n-2, -1, -1):                                         # Ux = b1 (
         s1 = 0
 print("The System is Solved Using MAQ = U")
 print("The Answer is:\n", x1)
-
+print("--------------------------------------")
 # PA = LU
 # Ax = b
 b2 = np.dot(PP, b)                                                    # b2 = pb
@@ -95,7 +96,7 @@ for i in range(1, n):                                                # Ly = b2 (
     if L[i, i] != 0:
         y[i] = (b2[i] - s2) / L[i, i]
         s2 = 0
-print("y", y)
+
 x2[n-1] = y[n-1]/U[n-1, n-1]
 for i in range(n-2, -1, -1):                                        # Ux = y (backward)
     for j in range(i+1, n):
